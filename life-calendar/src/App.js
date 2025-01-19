9153131c2dd525afd52fb6dd4418b436da295f2c
hg <chg9503@naver.com>
2025-01-19 21:03:18 +0900

    Add birthdate input

--- life-calendar/src/App.js
+++ life-calendar/src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [birthDate, setBirthDate] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    console.log("입력된 생년월일:", value);
+    setBirthDate(value);
+  }
+
+
+
   return (
     <div>
       <h1>인생달력</h1>
+      <div>
+        <label htmlFor="birthDate">생년월일: </label>
+        <input
+          type="date"
+          id="birthDate"
+          value={birthDate}
+          onChange={handleChange}
+        />
+      </div>
+      <p>입력된 생년월일: {birthDate} </p>
     </div>
   );
 }
